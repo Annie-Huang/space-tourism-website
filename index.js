9bd443bff847ec2bbc041fe67aa9da3c5c15745c
Annie Huang <annie.hn@gmail.com>
2022-11-06 14:18:45 +1100

    Add javascript to toggle attribute data-visible on .primary-navigation

---
+++ index.js
@@ -0,0 +1,19 @@
+// when someone clicks the hamburger button
+// if the nav is closed, open it
+// if the nav is open, close it.
+
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visiblity = nav.getAttribute('data-visible');
+
+  // attribute value is string
+  if (visiblity === 'false') {
+    nav.setAttribute('data-visible', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+  }
+
+  console.log(visiblity);
+});
