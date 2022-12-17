0331a7caeee9a062b191647862ee6d12ca9999b6
Annie Huang <annie.hn@gmail.com>
2022-12-17 18:09:39 +1100

    Refactor changeTabFocus a bit in tabs.js

--- tabs.js
+++ tabs.js
@@ -15,26 +15,24 @@ function changeTabFocus(e) {
   // change the tabindex of the current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute('tabindex', -1);
-  }
 
-  // if the right key is pushed, move to the next tab on the right
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      // if the left key is pushed, move to the next tab on the left
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
-  }
 
-  // if the left key is pushed, move to the next tab on the left
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
+    tabs[tabFocus].setAttribute('tabindex', 0);
+    tabs[tabFocus].focus();
   }
-
-  tabs[tabFocus].setAttribute('tabindex', 0);
-  tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
